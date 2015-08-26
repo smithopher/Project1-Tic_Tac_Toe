cf8435704b6be70da44245f4c5b4f51cd8d1944c
Christopher Smith <smithopher87@gmail.com>
2015-08-26 15:45:32 -0400

    defined moves for each player

---
+++ TTT.js
@@ -0,0 +1,66 @@
+$(function(){
+
+  $(function() {
+    $(".hidden").hide();
+  })
+
+  //Player X moves
+  // $("#square1").on("click", function() {
+  //   $("#square1x").show();
+  // })
+  // $("#square2").on("click", function() {
+  //   $("#square2x").show();
+  // })
+  // $("#square3").on("click", function() {
+  //   $("#square3x").show();
+  // })
+  // $("#square4").on("click", function() {
+  //   $("#square4x").show();
+  // })
+  // $("#square5").on("click", function() {
+  //   $("#square5x").show();
+  // })
+  // $("#square6").on("click", function() {
+  //   $("#square6x").show();
+  // })
+  // $("#square7").on("click", function() {
+  //   $("#square7x").show();
+  // })
+  // $("#square8").on("click", function() {
+  //   $("#square8x").show();
+  // })
+  // $("#square9").on("click", function() {
+  //   $("#square9x").show();
+  // })
+
+//player O moves
+// $("#square1").on("click", function() {
+//   $("#square1o").show();
+// })
+// $("#square2").on("click", function() {
+//   $("#square2o").show();
+// })
+// $("#square3").on("click", function() {
+//   $("#square3o").show();
+// })
+// $("#square4").on("click", function() {
+//   $("#square4o").show();
+// })
+// $("#square5").on("click", function() {
+//   $("#square5o").show();
+// })
+// $("#square6").on("click", function() {
+//   $("#square6o").show();
+// })
+// $("#square7").on("click", function() {
+//   $("#square7o").show();
+// })
+// $("#square8").on("click", function() {
+//   $("#square8o").show();
+// })
+// $("#square9").on("click", function() {
+//   $("#square9o").show();
+// })
+
+
+})
