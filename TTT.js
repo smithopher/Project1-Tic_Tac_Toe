e4cbf6a9f84ba807555085d9225c1f2cad701e27
Christopher Smith <smithopher87@gmail.com>
2015-08-27 16:42:43 -0400

    set winner logic

--- TTT.js
+++ TTT.js
@@ -14,7 +14,7 @@ $(function() {
        switch (moves % 2) {
          case 1:
            var xTurn = function() {
-             var xy = $(event.currentTarget).attr('id').replace("r","").split("c");
+             var xy = $(event.currentTarget).attr("id").replace("r","").split("c");
              var x = parseInt(xy[0]);
              var y = parseInt(xy[1]);
              gameBoard[x][y] = "1"
@@ -26,7 +26,7 @@ $(function() {
            break;
          case 0:
            var oTurn = function() {
-             var xy = $(event.currentTarget).attr('id').replace("r","").split("c");
+             var xy = $(event.currentTarget).attr("id").replace("r","").split("c");
              var x = parseInt(xy[0]);
              var y = parseInt(xy[1]);
              gameBoard[x][y] = "0"
@@ -37,8 +37,29 @@ $(function() {
            oTurn();
            break;
          }
-       })
-
+      var getWinner = function() {
+        if (((gameBoard[0][0] === "1") && (gameBoard[0][1] === "1") && (gameBoard[0][2] === "1"))||
+            ((gameBoard[1][0] === "1") && (gameBoard[1][1] === "1") && (gameBoard[1][2] === "1"))||
+            ((gameBoard[2][0] === "1") && (gameBoard[2][1] === "1") && (gameBoard[2][1] === "1"))||
+            ((gameBoard[0][0] === "1") && (gameBoard[1][0] === "1") && (gameBoard[2][0] === "1"))||
+            ((gameBoard[0][1] === "1") && (gameBoard[1][1] === "1") && (gameBoard[2][1] === "1"))||
+            ((gameBoard[0][2] === "1") && (gameBoard[1][2] === "1") && (gameBoard[2][2] === "1"))||
+            ((gameBoard[0][0] === "1") && (gameBoard[1][1] === "1") && (gameBoard[2][2] === "1"))||
+            ((gameBoard[0][2] === "1") && (gameBoard[1][1] === "1") && (gameBoard[2][0] === "1"))){
+              alert("Player 1 wins!!!");
+        } else if (((gameBoard[0][0] === "0") && (gameBoard[0][1] === "0") && (gameBoard[0][2] === "0"))||
+            ((gameBoard[1][0] === "0") && (gameBoard[1][1] === "0") && (gameBoard[1][2] === "0"))||
+            ((gameBoard[2][0] === "0") && (gameBoard[2][1] === "0") && (gameBoard[2][1] === "0"))||
+            ((gameBoard[0][0] === "0") && (gameBoard[1][0] === "0") && (gameBoard[2][0] === "0"))||
+            ((gameBoard[0][1] === "0") && (gameBoard[1][1] === "0") && (gameBoard[2][1] === "0"))||
+            ((gameBoard[0][2] === "0") && (gameBoard[1][2] === "0") && (gameBoard[2][2] === "0"))||
+            ((gameBoard[0][0] === "0") && (gameBoard[1][1] === "0") && (gameBoard[2][2] === "0"))||
+            ((gameBoard[0][2] === "0") && (gameBoard[1][1] === "0") && (gameBoard[2][0] === "0"))){
+              alert("Player 2 wins!!!");
+        }
     }
+    getWinner();
+  })
+}
   gamePlay();
 })
