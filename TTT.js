0c5d68007d2a7549d90a9dd6d8284148075d66d0
Christopher Smith <smithopher87@gmail.com>
2015-08-27 16:08:41 -0400

    added functionality to set value in gameboard to match interface

--- TTT.js
+++ TTT.js
@@ -9,34 +9,31 @@ $(function() {
     var gameBoard = [[null, null, null],
                      [null, null, null],
                      [null, null, null]];
-    $(".square").on("click", function() {
+    $(".square").on("click", function(event) {
+      console.log(gameBoard);
        switch (moves % 2) {
          case 1:
            var xTurn = function() {
-              //  var getX = $(event.currentTarget).find(".x");
-                //  getX.show();
-                //  var xy = $(event.currentTarget).id.replace("r","").split("c");
-                //  var x = parseInt(xy[0]);
-                //  var y = parseInt(xy[1]);
-                //  gameBoard[x][y] = "1"
-                 var getX = $(event.currentTarget).find(".x");
-                 getX.show();
-                 moves+=1;
-               }
+             var xy = $(event.currentTarget).attr('id').replace("r","").split("c");
+             var x = parseInt(xy[0]);
+             var y = parseInt(xy[1]);
+             gameBoard[x][y] = "1"
+             var getX = $(event.currentTarget).find(".x");
+             getX.show();
+             moves+=1;
+           }
            xTurn();
            break;
          case 0:
            var oTurn = function() {
-              //  var getO = $(event.currentTarget).find(".o");
-                //  getO.show();
-                //  var xy = $(event.currentTarget).id.replace("r","").split("c");
-                //  var x = parseInt(xy[0]);
-                //  var y = parseInt(xy[1]);
-                //  gameBoard[x][y] = "0"
-                 var getO = $(event.currentTarget).find(".o")
-                 getO.show();
-                 moves+=1;
-               }
+             var xy = $(event.currentTarget).attr('id').replace("r","").split("c");
+             var x = parseInt(xy[0]);
+             var y = parseInt(xy[1]);
+             gameBoard[x][y] = "0"
+             var getO = $(event.currentTarget).find(".o")
+             getO.show();
+             moves+=1;
+           }
            oTurn();
            break;
          }
