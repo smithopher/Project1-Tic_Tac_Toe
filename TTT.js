33cbdcd8cb8d64352ffd4d7e957dd11ebbfda8db
Christopher Smith <smithopher87@gmail.com>
2015-08-27 15:59:09 -0400

    refactored code to flow better and allow alternating moves

--- TTT.js
+++ TTT.js
@@ -1,66 +1,47 @@
 $(function(){
+  $(".o").hide();
+  $(".x").hide();
+});
 
-  $(function() {
-    $(".hidden").hide();
-  })
+$(function() {
+  var gamePlay = function() {
+    var moves = 1;
+    var gameBoard = [[null, null, null],
+                     [null, null, null],
+                     [null, null, null]];
+    $(".square").on("click", function() {
+       switch (moves % 2) {
+         case 1:
+           var xTurn = function() {
+              //  var getX = $(event.currentTarget).find(".x");
+                //  getX.show();
+                //  var xy = $(event.currentTarget).id.replace("r","").split("c");
+                //  var x = parseInt(xy[0]);
+                //  var y = parseInt(xy[1]);
+                //  gameBoard[x][y] = "1"
+                 var getX = $(event.currentTarget).find(".x");
+                 getX.show();
+                 moves+=1;
+               }
+           xTurn();
+           break;
+         case 0:
+           var oTurn = function() {
+              //  var getO = $(event.currentTarget).find(".o");
+                //  getO.show();
+                //  var xy = $(event.currentTarget).id.replace("r","").split("c");
+                //  var x = parseInt(xy[0]);
+                //  var y = parseInt(xy[1]);
+                //  gameBoard[x][y] = "0"
+                 var getO = $(event.currentTarget).find(".o")
+                 getO.show();
+                 moves+=1;
+               }
+           oTurn();
+           break;
+         }
+       })
 
-  //Player X moves
-  // $("#square1").on("click", function() {
-  //   $("#square1x").show();
-  // })
-  // $("#square2").on("click", function() {
-  //   $("#square2x").show();
-  // })
-  // $("#square3").on("click", function() {
-  //   $("#square3x").show();
-  // })
-  // $("#square4").on("click", function() {
-  //   $("#square4x").show();
-  // })
-  // $("#square5").on("click", function() {
-  //   $("#square5x").show();
-  // })
-  // $("#square6").on("click", function() {
-  //   $("#square6x").show();
-  // })
-  // $("#square7").on("click", function() {
-  //   $("#square7x").show();
-  // })
-  // $("#square8").on("click", function() {
-  //   $("#square8x").show();
-  // })
-  // $("#square9").on("click", function() {
-  //   $("#square9x").show();
-  // })
-
-//player O moves
-// $("#square1").on("click", function() {
-//   $("#square1o").show();
-// })
-// $("#square2").on("click", function() {
-//   $("#square2o").show();
-// })
-// $("#square3").on("click", function() {
-//   $("#square3o").show();
-// })
-// $("#square4").on("click", function() {
-//   $("#square4o").show();
-// })
-// $("#square5").on("click", function() {
-//   $("#square5o").show();
-// })
-// $("#square6").on("click", function() {
-//   $("#square6o").show();
-// })
-// $("#square7").on("click", function() {
-//   $("#square7o").show();
-// })
-// $("#square8").on("click", function() {
-//   $("#square8o").show();
-// })
-// $("#square9").on("click", function() {
-//   $("#square9o").show();
-// })
-
-
+    }
+  gamePlay();
 })
