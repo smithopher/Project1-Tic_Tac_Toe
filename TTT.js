c9b94539b8bf8e197f79bdb25f94c856a1cc178a
Christopher Smith <smithopher87@gmail.com>
2015-08-28 13:54:16 -0400

    updated to not allow players to select same squares

--- TTT.js
+++ TTT.js
@@ -1,25 +1,31 @@
-$(function(){
-  $(".o").hide();
+$(function(){      //since I pre-filled each square with each svg symbol I need to hide them.
+  $(".o").hide();  //I set this in a separate function from actual gameplay so it loads faster.
   $(".x").hide();
 });
 
 $(function() {
-  var gamePlay = function() {
+  var gamePlay = function() {  // Entire game contained in here
+    var player = $("#player");
     var moves = 1;
     var gameBoard = [[null, null, null],
                      [null, null, null],
                      [null, null, null]];
     $(".square").on("click", function(event) {
-       switch (moves % 2) {
+       switch (moves % 2) { //switch to alternate player by click
          case 1:
            var xTurn = function() {
              var xy = $(event.currentTarget).attr("id").replace("r","").split("c");
              var x = parseInt(xy[0]);
              var y = parseInt(xy[1]);
-             gameBoard[x][y] = "1"
-             var getX = $(event.currentTarget).find(".x");
-             getX.show();
-             moves+=1;
+             if (gameBoard [x][y] === null) { // only make move if square is not taken
+               gameBoard[x][y] = "1"
+               var getX = $(event.currentTarget).find(".x");
+               getX.show();
+               moves+=1;
+             } else { // prevent from over-writing gameBoard value and showing both icons in display
+               alert ("Square is already taken! Choose another square!!!");
+             }
+             player.text("2");
            }
            xTurn();
            break;
@@ -28,10 +34,15 @@ $(function() {
              var xy = $(event.currentTarget).attr("id").replace("r","").split("c");
              var x = parseInt(xy[0]);
              var y = parseInt(xy[1]);
-             gameBoard[x][y] = "0"
-             var getO = $(event.currentTarget).find(".o")
-             getO.show();
-             moves+=1;
+             if (gameBoard[x][y] === null){
+               gameBoard[x][y] = "0"
+               var getO = $(event.currentTarget).find(".o")
+               getO.show();
+               moves+=1;
+             } else {
+               alert("Square is already taken! Choose another square!!!");
+             }
+             player.text("1");
            }
            oTurn();
            break;
@@ -39,7 +50,7 @@ $(function() {
       var getWinner = function() {
         if (((gameBoard[0][0] === "1") && (gameBoard[0][1] === "1") && (gameBoard[0][2] === "1"))||
             ((gameBoard[1][0] === "1") && (gameBoard[1][1] === "1") && (gameBoard[1][2] === "1"))||
-            ((gameBoard[2][0] === "1") && (gameBoard[2][1] === "1") && (gameBoard[2][1] === "1"))||
+            ((gameBoard[2][0] === "1") && (gameBoard[2][1] === "1") && (gameBoard[2][2] === "1"))||
             ((gameBoard[0][0] === "1") && (gameBoard[1][0] === "1") && (gameBoard[2][0] === "1"))||
             ((gameBoard[0][1] === "1") && (gameBoard[1][1] === "1") && (gameBoard[2][1] === "1"))||
             ((gameBoard[0][2] === "1") && (gameBoard[1][2] === "1") && (gameBoard[2][2] === "1"))||
@@ -53,10 +64,11 @@ $(function() {
               }
               $(".x").hide();
               $(".o").hide();
-              moves = 1;
+              moves = 1
+              player.text("1");
         } else if (((gameBoard[0][0] === "0") && (gameBoard[0][1] === "0") && (gameBoard[0][2] === "0"))||
             ((gameBoard[1][0] === "0") && (gameBoard[1][1] === "0") && (gameBoard[1][2] === "0"))||
-            ((gameBoard[2][0] === "0") && (gameBoard[2][1] === "0") && (gameBoard[2][1] === "0"))||
+            ((gameBoard[2][0] === "0") && (gameBoard[2][1] === "0") && (gameBoard[2][2] === "0"))||
             ((gameBoard[0][0] === "0") && (gameBoard[1][0] === "0") && (gameBoard[2][0] === "0"))||
             ((gameBoard[0][1] === "0") && (gameBoard[1][1] === "0") && (gameBoard[2][1] === "0"))||
             ((gameBoard[0][2] === "0") && (gameBoard[1][2] === "0") && (gameBoard[2][2] === "0"))||
@@ -70,9 +82,22 @@ $(function() {
               }
               $(".x").hide();
               $(".o").hide();
-              moves = 1;
+              moves = 1
+              player.text("1");
+        } else if (moves === 10){
+          alert("Gridlock! Try again!");
+          for(var i = 0; i < gameBoard.length; i++){
+            for(var n = 0; n < gameBoard[i].length; n++){
+              gameBoard[i][n] = null;
+            }
+          }
+          $(".x").hide();
+          $(".o").hide();
+          moves = 1
+          player.text("1");
         }
       }
+      console.log(gameBoard);
       getWinner();
     })
   }
